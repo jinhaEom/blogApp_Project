ab83c44c0ca5372f56993a432a68dd9ad46f4b49
jinahaEom <umjin98@naver.com>
2022-12-18 21:53:25 +0900

    로그인 화면 구성

---
+++ README.md
@@ -0,0 +1,3 @@
+## MVVM 패턴을 이용한 블로그 & 다이어리 앱 개인 프로젝트
+
+<img src="https://user-images.githubusercontent.com/84216838/208299406-2111d5ae-9e87-4117-a75e-2c7d2faa2c01.png" width = 230 height = 350>
---
+++ app/src/main/java/bu/ac/kr/blogapp/data/blogModel.kt
@@ -0,0 +1,29 @@
+package bu.ac.kr.blogapp.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="tb_blog")
+data class blogModel(
+
+    @PrimaryKey(autoGenerate = true)
+    val id : Long?,
+
+    @ColumnInfo(name = "seq")
+    var seq : Int,
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "content")
+    var content: String,
+
+    @ColumnInfo(name= "image")
+    var image : String,
+
+    @ColumnInfo(name = "createDate")
+    var createDate: Long
+){
+    constructor(): this(null,0,"","","",-1)
+}
